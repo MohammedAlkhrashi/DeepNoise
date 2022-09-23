88f8b736de8a92f7b3717f547f1c9cde0ce5147c
Hisham Alyahya <Hishamaalyahya@gmail.com>
2022-09-23 19:29:29 +0300

    Fix loss correction

--- loss_correction.py
+++ loss_correction.py
@@ -6,55 +6,72 @@ from torch.utils.data import DataLoader
 import torch.nn.functional as F
 from erm import ERM
 from callbacks import Callback
-import numpy as np
 
 
 class BackwardCorrectedLoss(Module):
+    # TODO: add docstring
     def __init__(self, T: List[List[float]]):
         super(BackwardCorrectedLoss, self).__init__()
         self.T = T
 
-    def forward(self, y_pred, y):
-        T_inv = torch.Tensor(np.linalg.inv(self.T))
-        return -torch.sum(torch.dot(y, T_inv) * torch.log(y_pred))
+    def forward(self, pred, target):
+        T_inv = torch.linalg.inv(self.T).float()
+        num_classes = pred.size(-1)
+        assert (
+            len(T_inv) == num_classes
+        ), "T matrix must be in the form NxN, where N is the number of classes."
+        one_hot_target = F.one_hot(target, num_classes=num_classes).float()
+        pred_prop = F.softmax(pred, dim=1)
+        return -torch.sum(torch.matmul(one_hot_target, T_inv) * torch.log(pred_prop))
 
 
 class ForwardCorrectedLoss(Module):
     def __init__(self, T: List[List[float]]):
         super(ForwardCorrectedLoss, self).__init__()
+        # TODO: check validitiy of T
         self.T = T
 
-    def forward(self, y_pred, y):
-        T = torch.Tensor(self.T)
-        return -torch.sum(y * torch.log(torch.dot(y_pred, T)))
+    def forward(self, pred, target):
+        T = torch.Tensor(self.T).float()
+        num_classes = pred.size(-1)
+        assert (
+            len(T) == num_classes
+        ), "T matrix must be in the form NxN, where N is the number of classes."
+        one_hot_target = F.one_hot(target, num_classes=num_classes).float()
+        pred_prop = F.softmax(pred, dim=1)
+        return -torch.sum(one_hot_target * torch.log(torch.matmul(pred_prop, T)))
 
 
 class LossCorrectionTrainer(ERM):
     def __init__(
-		self,
-		model: Module,
-		optimizer: Optimizer,
-		train_loader: DataLoader,
-		val_loader: DataLoader,
-		test_loader: DataLoader,
-		epochs: int,
-		correction: str = 'backward',
-        T: List[List[float]],
-		callbacks: List[Callback] = None,
-	) -> None:
+        self,
+        model: Module,
+        optimizer: Optimizer,
+        train_loader: DataLoader,
+        val_loader: DataLoader,
+        test_loader: DataLoader,
+        epochs: int,
+        T,
+        correction: str = "backward",
+        callbacks: List[Callback] = None,
+    ) -> None:
 
-		if correction == 'backward':
-			loss_fn = BackwardCorrectedLoss(T)
-		else:
-			loss_fn = ForwardCorrectedLoss(T)
-		
-		super().__init__(
-			model,
-			optimizer,
-			loss_fn,
-			train_loader,
-			val_loader,
-			test_loader,
-			epochs,
-			callbacks,
-		)
+        if correction == "backward":
+            loss_fn = BackwardCorrectedLoss(T)
+        elif correction == "forward":
+            loss_fn = ForwardCorrectedLoss(T)
+        else:
+            raise ValueError(
+                f'Correction type {correction} unrecognized. Pass either "forward" or "backward"'
+            )
+
+        super().__init__(
+            model,
+            optimizer,
+            loss_fn,
+            train_loader,
+            val_loader,
+            test_loader,
+            epochs,
+            callbacks,
+        )
